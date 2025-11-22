c7054344f6612be3006e23c37da54b115f4920fc
kevinzhangmk <kevinzhangmk@gmail.com>
2025-11-22 16:48:00 -0600

    updated files

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()); // parse JSON in requests
+app.use(express.static(__dirname));
 
 app.post('/add-word', (req, res) => {
     const newWord = req.body.word;
    
-    const data = JSON.parse(fs.readFileSync("inputs.json")); //read json
+    const data = JSON.parse(fs.readFileSync("input.json")); //read json
 
     const existing = data.find(w => w.word === newWord);
     if (existing) {
@@ -16,9 +17,14 @@ app.post('/add-word', (req, res) => {
         data.push({ word: newWord, value: 1 });
     }
 
-    fs.writeFileSync('words.json', JSON.stringify(data, null, 2));
+    fs.writeFileSync('input.json', JSON.stringify(data, null, 2));
 
     res.send({ success: true, data: data });
+});
+
+app.get('/get_words', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('input.json'));
+    res.json(data);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
